2c124a7edf31f342266b525e747d7f1692901d78
Nick Hodges <nickhodges@gmail.com>
2024-08-19 06:21:23 -0400

    Initial commit

---
+++ run.sh
@@ -0,0 +1,12 @@
+
+
+#!/bin/bash
+
+# Print a message to indicate the script is starting
+echo "Starting the Express app..."
+
+# Run the TypeScript Express app using ts-node
+npx ts-node src/index.ts
+
+# Print a message to indicate the script has finished
+echo "Express app has stopped."
